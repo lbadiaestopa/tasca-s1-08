94c357e1a8bdf68b93591bac1fc51be80197bb45
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-28 20:29:08 +0100

    feat: add testCanCreateBook test

---
+++ nivell-3/exercici-1/tests/BookTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/Book.php';
+
+class BookTest extends TestCase
+{
+    public function testCanCreateBook()
+    {
+        $book = new Book(
+        "1984",
+        "George Orwell",
+        "1234567890",
+        "Distopia",
+        328
+    );
+        $this->assertSame("1984", $book->getTitle());
+        $this->assertEquals("George Orwell", $book->getAuthor());
+        $this->assertEquals("1234567890", $book->getISBN());
+        $this->assertEquals("Distopia", $book->getGenre());
+        $this->assertEquals(328, $book->getPages());
+    }
+}

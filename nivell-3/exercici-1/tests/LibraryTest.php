aaabd222c521a53188f1d2e23e9709db729eefdf
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-01 11:48:16 +0100

    feat: add tesCanAddBooks and testCanRemoveBooks

--- nivell-3/exercici-1/tests/LibraryTest.php
+++ nivell-3/exercici-1/tests/LibraryTest.php
@@ -4,12 +4,35 @@ use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Library.php';
+require_once __DIR__ . '/../src/Book.php';
 
 class LibraryTest extends TestCase
 {
     public function testCanCreateEmptyLibrary()
     {
         $library = new Library();
-        $this->assertEmpty($library->getAllBooks());
+        $this->assertEmpty($library->countBooks());
     }
+
+    public function testCanAddBooks()
+    {
+        $library = new Library();
+        $book = new Book("1984", "George Orwell", "1234567890", Genre::Distopia, 328);
+
+        $library->addBook($book);
+
+        $this->assertSame(1, $library->countBooks());
+    }
+
+    public function testCanRemoveBooks()
+    {
+        $library = new Library();
+        $book = new Book("1984", "George Orwell", "1234567890", Genre::Distopia, 328);
+
+        $library->addBook($book);
+        $library->removeBook(0);
+
+        $this->assertSame(0, $library->countBooks());
+    }
+
 }
